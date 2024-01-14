87cc45f5cc8eeee91edadeeedc3360fbe562f95d
Protyasha roy <protyasharoy269@gmail.com>
2024-01-15 01:27:06 +0600

    added new pages

--- unitrack-client/src/App.js
+++ unitrack-client/src/App.js
@@ -1,6 +1,13 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import AddStudent from './pages/AddStudent/AddStudent';
+import Attendance from './pages/Attendance/Attendance';
+import SendMails from './pages/SendMails/SendMails';
+import StudentsList from './pages/StudentsList/StudentsList';
+import AttendanceList from './pages/AttendanceList/AttendanceList';
+import AllMails from './pages/AllMails/AllMails';
+import UserProfile from './pages/UserProfile/UserProfile';
 
 function App() {
   return (
@@ -8,6 +15,13 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/addStudent' element={<AddStudent />} />
+      <Route path='/attendance' element={<Attendance />} />
+      <Route path='/sendMails' element={<SendMails />} />
+      <Route path='/studentsList' element={<StudentsList />} />
+      <Route path='/attendanceList' element={<AttendanceList />} />
+      <Route path='/allMails' element={<AllMails />} />
+      <Route path='/userProfile' element={<UserProfile />} />
     </Routes>
   </Router>
   );
--- unitrack-client/src/components/HomeDash/HomeDash.js
+++ unitrack-client/src/components/HomeDash/HomeDash.js
@@ -1,7 +1,43 @@
 import React from 'react'
+import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import {
+   UserOutlined
+  } from '@ant-design/icons';
 
 export default function HomeDash() {
+    const navigate = useNavigate();
+
+    const handleLogOut = () => {
+        console.log('loged out');
+    }
+
   return (
-    <div>HomeDash</div>
+    <section>
+        <h2 className='headingText'><span className='appName'>UniTrack</span> - Student Management System</h2>
+        <div className='container'>
+                <div className='container pointer'>
+                    <div onClick={() => navigate('/userProfile')} className='border p-5 m-5 container'>
+                        <UserOutlined />
+                        <p>Even bigger name, bigger than anything</p>
+                    </div>
+                    <Button className='w-full' onClick={handleLogOut}>Log Out</Button>
+                </div>
+                <div className='flex gap-10'>
+                    <div className='container p-10 justify-between gap-10'>
+                        <h3 className='textCenter'>Actions List</h3>
+                        <Button className='w-full' onClick={() => navigate('/addStudent')}>Add new student</Button>
+                        <Button className='w-full' onClick={() => navigate('/attendance')}>Take Attendance</Button>
+                        <Button className='w-full' onClick={() => navigate('/sendMails')}>Send Multiple Mail</Button>
+                    </div>
+                    <div className='container p-10 justify-between gap-10'>
+                        <h3 className='textCenter'>Saved Data</h3>
+                        <Button className='w-full' onClick={() => navigate('/studentsList')}>Students List</Button>
+                        <Button className='w-full' onClick={() => navigate('/attendanceList')}>Attendance List</Button>
+                        <Button className='w-full' onClick={() => navigate('/allMails')}>Sent Mails</Button>
+                    </div>
+                </div>
+            </div>
+    </section>
   )
 }
