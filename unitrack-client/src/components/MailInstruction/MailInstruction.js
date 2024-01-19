b7429863a181508fe9df49680b9b6ceca607e6a1
Protyasha roy <protyasharoy369@gmail.com>
2024-01-19 19:46:47 +0600

    instructions updated

--- unitrack-client/src/components/MailInstruction/MailInstruction.js
+++ unitrack-client/src/components/MailInstruction/MailInstruction.js
@@ -6,7 +6,7 @@ const { Text } = Typography;
 export default function MailInstruction() {
   return (
     <section>
-        <Text>To get your app-password:</Text>
+        <Text>To get your SMTP key:</Text>
         <ul>
             <li><Text>Create an account/login in <a target="_blank" href='https://onboarding.brevo.com/account/register'>Brevo</a></Text></li>
             <li><Text>Go to this page <a target="_blank" href='https://app.brevo.com/settings/keys/smtp'>SMTP & API</a></Text></li>
